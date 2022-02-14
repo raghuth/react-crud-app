ca51dc03a65805525ec6043b8120de1556c8c68a
ragahvendra <raghavendra@rivetsys.com>
2022-02-14 20:34:21 +0530

    Added code

--- models/todoListModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const todoListScheme = {
-  name: { type: String, require: true },
-};
-
-const TodoList = mongoose.model("TodoList", todoListScheme);
-
-module.exports = TodoList;
